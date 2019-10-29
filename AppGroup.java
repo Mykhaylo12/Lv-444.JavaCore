072541363e9c9d0de3e32734e8281b5834f2f8c7
KryvenkoSergii <kivenko.sergey@gmail.com>
2019-10-29 16:52:22 +0200

    create project

---
+++ AppGroup.java
@@ -0,0 +1,78 @@
+package classwork13.kryvenkosergii_lv44;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import classwork4.kryvenkosergii_lv44.Continents;
+
+public class AppGroup {
+
+	public static void main(String[] args) throws IOException {
+		int num;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter number of task");
+		String text = br.readLine();
+		
+		switch (text) {
+		case "1":
+			System.out.println("");
+			break;
+			
+		case "2":
+			System.out.println("");
+			break;
+			
+		case "3":
+			System.out.println("");
+			break;
+			
+		case "4":
+			System.out.println("");
+			break;
+			
+		case "5":
+			System.out.println("");
+			break;
+			
+		case "6":
+			System.out.println("");
+			break;
+			
+		case "7":
+			System.out.println("");
+			break;
+			
+		case "8":
+			System.out.println("");
+			break;
+			
+		case "9":
+			System.out.println("");
+			break;
+			
+		case "10":
+			System.out.println("");
+			break;
+			
+		case "11":
+			System.out.println("");
+			break;
+			
+		case "12":
+			System.out.println("");
+			break;
+			
+		case "13":
+			System.out.println("");
+			break;
+			
+		case "14":
+			System.out.println("");
+			break;
+			
+		}
+
+	}
+
+}
