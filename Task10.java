0b42207f0cedd6afd428a4e4f1bc2fd859b4f3b5
greatpanda1603 <olesya.ostriychuk@gmail.com>
2019-10-29 18:50:16 +0200

    add task 10

---
+++ Task10.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task10 {
+
+	public static void findNumberOfStepsToGetOne() {
+
+		Long randomLong = ThreadLocalRandom.current().nextLong(1, Long.MAX_VALUE);
+		System.out.println(randomLong);
+		int counter = 0;
+		while (randomLong != 1) {
+			if (randomLong % 2 == 0) {
+				randomLong = randomLong / 2;
+			} else {
+				randomLong = randomLong * 3 + 1;
+			}
+			counter++;
+		}
+		System.out.println(counter);
+	}
+}
